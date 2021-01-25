2561e82edc6a932db89b273c2b4fbc6256176222
Josh Soref <jsoref@users.noreply.github.com>
2021-01-25 08:16:08 -0500

    spelling: been
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- tests/robot-cases/Group3-Upgrade/prepare.py
+++ tests/robot-cases/Group3-Upgrade/prepare.py
@@ -181,7 +181,7 @@ class HarborAPI:
                                 print(body)
                                 request(url+"projects/"+project["name"]+"/preheat/policies", 'post', **body)
                         else:
-                            raise Exception(r"Please verify if distribution {} has beed created.".format(policy["provider_name"]))
+                            raise Exception(r"Please verify if distribution {} has been created.".format(policy["provider_name"]))
         else:
             raise Exception(r"Error: Feature {} has no branch {}.".format(sys._getframe().f_code.co_name, branch))
 
